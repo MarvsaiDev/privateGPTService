65a6bc980ca3b29926b0780f5a870cd1d5767cce
M ghandi <mahmoud@marvsai.com>
2024-03-19 01:27:06 -0400

    fix to startup script

--- startup.sh
+++ startup.sh
@@ -2,8 +2,11 @@
 #echo hi > /home/hellofromapp2.txt
 val=$(python findpath.py)
 echo $val
-cd $val
+pushd $val  # This will navigate to the directory stored in 'val' and save the current directory.
 mv langchainmsai langchain
+popd
+#cd $val
+#mv langchainmsai langchain
 # find /home/user_files -name "*.db" -type f -mtime +3 -exec rm {} \;
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker service:app
 
