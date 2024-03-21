ddbbc4661d860c0c2e44cae3937d58fc17f69971
M ghandi <mahmoud@marvsai.com>
2024-03-21 02:17:13 -0400

    sqlite update

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 pip install -r requirements.txt
 echo hi > /home/hellofromapp2.txt
 val=$(python findpath.py)
@@ -8,8 +9,4 @@ echo $val
 cd $val  # This will navigate to the directory stored in 'val' and save the current directory.
 mv langchainmsai langchain
 cd $cdir
-#cd $val
-#mv langchainmsai langchain
-# find /home/user_files -name "*.db" -type f -mtime +3 -exec rm {} \;
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker service:app
-
