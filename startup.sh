1614167cb08f38d064b4c325a5f6c8c08392f064
M ghandi <mahmoud@marvsai.com>
2024-03-21 03:24:21 -0400

    sqlite update

--- startup.sh
+++ startup.sh
@@ -9,4 +9,4 @@ echo $val
 cd $val  # This will navigate to the directory stored in 'val' and save the current directory.
 mv langchainmsai langchain
 cd $cdir
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker service:app
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker service:app
